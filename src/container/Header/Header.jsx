05d899f4465f03a168aa77528b91cbd3b6e5ad19
Samridhi Duggal <duggalsamridhi0711@gmail.com>
2024-01-29 00:23:29 +0530

    header updated

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading />
+      <h1 className='app__header-h1'> The Key to Fine Dining</h1>
+    </div>
+
+    <div className="app__wrapper_img">
+
+    </div>
   </div>
 );
 
